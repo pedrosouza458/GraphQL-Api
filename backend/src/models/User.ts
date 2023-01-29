590a443cd32d1b1f5c9f434470abdefb2b91772d
Pedro Souza <pedrosouza.ch453@academico.ifsul.edu.br>
2023-01-29 02:03:02 -0300

    .

---
+++ backend/src/models/User.ts
@@ -0,0 +1,12 @@
+import { Field, ID, ObjectType } from "type-graphql";
+
+@ObjectType()
+export class User {
+
+   @Field(_type => ID)
+   id: string
+
+   @Field((type)=> String) 
+   name: string
+}
+
